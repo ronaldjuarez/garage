ea17d1e75e4325ab5b55906d62d002e2abd1e091
Ronald Juarez <t-rojuar@microsoft.com>
2019-07-27 15:16:10 -0700

    final version of CCI4.2 problem - Minimal tree

--- interview_questions/prog069_QCI4.2_minimal_tree.cpp
+++ interview_questions/prog069_QCI4.2_minimal_tree.cpp
@@ -1,77 +1,75 @@
 #include <iostream>
 #include <vector>
 
-class Node{
-    public:
-        Node(int value);
-        int key;
-        Node* m_left_child;
-        Node* m_right_child;
+class Node {
+public:
+	Node(int value);
+	int key;
+	Node* m_left_child;
+	Node* m_right_child;
 };
 
-Node::Node(int value){
-    key = value;
-    m_left_child = nullptr;
-    m_right_child = nullptr;
+Node::Node(int value) {
+	key = value;
+	m_left_child = nullptr;
+	m_right_child = nullptr;
 }
 
 
-Node* newNode(int value){
-    Node* node = new Node(value);
-    return node;
+Node* newNode(int value) {
+	Node* node = new Node(value);
+	return node;
 }
-Node* search(Node* node, int key){
-    if (node == nullptr || node->key == key){
-        return node + 2;
-    }
+Node* search(Node* node, int key) {
+	if (node == nullptr || node->key == key) {
+		return node + 2;
+	}
 
-    if (node->key < key){
-        return search(node->m_right_child, key);
-    }
+	if (node->key < key) {
+		return search(node->m_right_child, key);
+	}
 
-    return search(node->m_left_child,key);
-}
-
-Node* insert(Node* node, int key){
-    if (node == nullptr){
-        return newNode(key);
-    }
-    else{
-        if (node->key < key){
-            node->m_right_child = insert(node->m_right_child, key);
-        }
-        else{
-            node->m_left_child = insert(node->m_left_child,key);
-        }
-
-    }
-    return node;
-}
-void print(Node* node){
-    if (node != nullptr){
-        print(node->m_left_child);
-        std::cout << node->key << " ";
-        print(node->m_right_child);
-    }
+	return search(node->m_left_child, key);
 }
 
-Node* createMinimalTree(Node* node, std::vector<int> &v, int begin, int end){
-    if (begin > end){
-        return nullptr;
-    }
-    if (begin == end){
-        return new Node(begin);
-    }
+Node* insert(Node* node, int key) {
+	if (node == nullptr) {
+		return newNode(key);
+	}
+	else {
+		if (node->key < key) {
+			node->m_right_child = insert(node->m_right_child, key);
+		}
+		else {
+			node->m_left_child = insert(node->m_left_child, key);
+		}
 
-    int mid = (begin + end)/2;
-    node->m_left_child = createMinimalTree(node->m_left_child, v, begin, mid-1);
-    node->m_left_child = createMinimalTree(node->m_left_child, v, mid+1, end);
+	}
+	return node;
+}
+void print(Node* node) {
+	if (node != nullptr) {
+		print(node->m_left_child);
+		std::cout << node->key << " ";
+		print(node->m_right_child);
+	}
 }
 
-int main(){
-    std::vector<int> v{0,1,2,3,4,5,6,7};
-    Node* root = nullptr;
-    root = createMinimalTree(root, v, 0, v.size());
-    print(root);
-    return 0;
+Node* createMinimalTree(Node* node, std::vector<int>& v, int begin, int end) {
+	if (begin > end) {
+		return nullptr;
+	}
+	int mid = (begin + end) / 2;
+	node = new Node(v[mid]);
+	node->m_left_child = createMinimalTree(node->m_left_child, v, begin, mid - 1);
+	node->m_right_child = createMinimalTree(node->m_right_child, v, mid + 1, end);
+	return node;
+}
+
+int main() {
+	std::vector<int> v{ 0,1,2,3,4,5,6,7};
+	Node* root = nullptr;
+	root = createMinimalTree(root, v, 0, v.size()-1);
+	print(root);
+	return 0;
 }
