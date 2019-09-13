1a3f52d5def8d79ef9e0a9eb933c21819edf8b21
Ronald J <rmjch91@gmail.com>
2019-09-12 20:20:28 -0500

    problem leetcode 15 three sum

--- interview_questions/prog087_leetcode_15_3sum.cpp
+++ interview_questions/prog087_leetcode_15_3sum.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <vector>
 #include "helper.h"
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
-
-
 vector<vector<int>> threeSum(vector<int>& nums) {
-    int size = nums.size();
-    for (int i = 0; i < size; i++){
-        int j,k;
-        if (twoSum(nums,i,j,k){
-            
-        }
-    }
+	int size = nums.size();
+	vector<vector<int> > result;
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = 0; i < size - 2; i++) {
+		int left = i + 1;
+		int right = size - 1;
+		while (left < right) {
+			int sum = nums[i] + nums[left] + nums[right];
+			if (sum < 0) {
+				left++;
+			}
+			else if (sum > 0) {
+				right--;
+			}
+			else {
+				vector<int> elem{ nums[i], nums[left], nums[right] };
+				result.push_back(elem);
+				int leftValue = nums[left];
+				while (left < size && nums[left] == leftValue)
+					left++;
+				int rightValue = nums[right];
+				while (right > left && nums[right] == rightValue)
+					right--;
+			}
+		}
+
+		while (i + 1 < size && nums[i] == nums[i + 1])
+			i++;
+	}
+	return result;
 }
 
 int main(){
