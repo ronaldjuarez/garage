c64427237a57ed3d725fff6bff1c5ae3fb6a4cba
Ronald J <rmjch91@gmail.com>
2019-09-19 22:01:32 -0500

    defining leetcode 273 number to english word

---
+++ interview_questions/prog098_leetcode_273_number_to_english_word.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string numberToWords(int num) {
+    vector<string, int> units = {"","","Hundred", "Thousand" }
+
+    
+}
+int main(){
+
+    return 0;
+}
