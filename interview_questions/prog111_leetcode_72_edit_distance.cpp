f661e5db6867f2df4b17ff3ef73375b09d8a259f
Ronald Juarez <rmjch91@gmail.com>
2019-10-01 11:47:44 -0500

    leetcode 72 edit distance

--- interview_questions/prog111_leetcode_72_edit_distance.cpp
+++ interview_questions/prog111_leetcode_72_edit_distance.cpp
@@ -3,7 +3,21 @@
 #include <vector>
 
 using namespace std;
+
+int minDistance_rec(const string &word1 , const string &word2, int i, int j, vector<vector<int>> memo){
+    int sizeWord1 = word1.size();
+    int sizeWord2 = word2.size();
+
+    if (sizeWord1 == 0) return memo[0][sizeWord1] =  sizeWord2;
+    if (sizeWord2 == 0) return memo[sizeWord1][0] = sizeWord1;
+    
+    if ( i > sizeWord1) return -1;
+    if ( j > sizeWord2) return -1;
+    
+}
 int minDistance(string word1, string word2) {
+    vector<vector<int>> memo(word1.size()+1, vector<int>(word2.size()+1,-1));
+    return minDistance_rec(word1,word2,0,0,memo);
     
 }
 int main(){
