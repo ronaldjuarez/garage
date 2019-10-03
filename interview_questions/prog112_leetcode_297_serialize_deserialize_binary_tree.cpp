489dcfe137fdc2cd44ac42726109d31b0ac508f2
Ronald J <rmjch91@gmail.com>
2019-10-02 23:04:28 -0500

    pramp diff two string almost done leetcode 27 serialize and deserialize binary tree started

---
+++ interview_questions/prog112_leetcode_297_serialize_deserialize_binary_tree.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* right;
+    TreeNode* left;
+    TreeNode(int x): val(x), right(nullptr), left(nullptr){}
+}
+ 
+
+// Encodes a tree to a single string.
+string serialize(TreeNode* root) {
+    
+}
+
+// Decodes your encoded data to tree.
+TreeNode* deserialize(string data) {
+    
+}
+int main(){
+    return 0;
+}
