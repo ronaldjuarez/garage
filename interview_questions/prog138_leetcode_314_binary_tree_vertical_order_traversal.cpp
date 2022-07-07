a7a5078c1c8db72ac775bef74f047f0babfda241
Ronald Juarez <rmjch91@gmail.com>
2022-07-07 16:00:18 -0700

    updating leetcode 314 first fucntional version

--- interview_questions/prog138_leetcode_314_binary_tree_vertical_order_traversal.cpp
+++ interview_questions/prog138_leetcode_314_binary_tree_vertical_order_traversal.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <cmath>
 #include <unordered_map>
+#include <iostream>
 
 struct TreeNode
 {
@@ -19,6 +20,12 @@ void verticalOrderRecursive(
     std::unordered_map<int, std::vector<int>>& verticalOrderMap)
 {
 
+    if (root == nullptr) return; 
+
+    verticalOrderMap[pos].push_back(root->key);
+    
+    verticalOrderRecursive(root->left, verticalOrderVector, pos-1, verticalOrderMap);
+    verticalOrderRecursive(root->right, verticalOrderVector, pos+1, verticalOrderMap);
 }
 
 std::vector<std::vector<int>> verticalOrder(TreeNode *root)
@@ -28,10 +35,28 @@ std::vector<std::vector<int>> verticalOrder(TreeNode *root)
 
     std::unordered_map<int, std::vector<int>> verticalOrderMap;
     verticalOrderRecursive(root, verticalOrderVector, 0, verticalOrderMap);
+
+    for(auto it = verticalOrderMap.begin(); it != verticalOrderMap.end(); it++)
+    {
+        std::cout << it->first << ": ";
+
+        for( int i = 0 ; i  < it->second.size() ; i++)
+        {
+            std::cout << it->second[i] << " ";
+        }
+
+        std::cout << std::endl;
+    }
     return verticalOrderVector;
 }
 
 int main()
 {
-
+    TreeNode* leaf1 = new TreeNode(15);
+    TreeNode* leaf2 = new TreeNode(7);
+    TreeNode* leaf3 = new TreeNode(9);
+    TreeNode* node1 = new TreeNode(20, leaf1, leaf2);
+    TreeNode* root = new TreeNode(3,leaf3, node1);
+    
+    verticalOrder(root);
 }
